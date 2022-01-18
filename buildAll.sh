a2a80f8f7afca3f2914b3383585992e1008c789b
mtoslalibu <44147008+mtoslalibu@users.noreply.github.com>
2022-01-18 10:21:15 -0500

    Create buildAll script

---
+++ buildAll.sh
@@ -0,0 +1,17 @@
+cd /local/java-web-servlet-filter
+git pull
+sudo ./mvnw clean install -Dlicense.skip=true -Dcheckstyle.skip -DskipTests=true
+
+cd /local/java-spring-web
+git pull
+sudo ./mvnw clean install -Dlicense.skip=true -Dcheckstyle.skip -DskipTests=true
+
+cd /local/java-spring-jaeger
+git pull
+sudo ./mvnw clean install -Dlicense.skip=true -Dcheckstyle.skip -DskipTests=true
+
+cd /local/train-ticket
+sudo mvn clean package -Dmaven.test.skip=true
+sudo docker-compose build
+
+sudo docker-compose up
