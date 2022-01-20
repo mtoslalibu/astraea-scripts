e98bb3e0e5be608bfc4c6241ab48de4bd67db2ff
mtoslalibu <44147008+mtoslalibu@users.noreply.github.com>
2022-01-20 11:28:23 -0500

    Fix hey workload

--- heySendRequest.sh
+++ heySendRequest.sh
@@ -1,1 +1,1 @@
-hey -z 30s -c 10 -m POST -H "Content-Type: application/json" -d '{"startingPlace": "Shang Hai", "endPlace": "Su Zhou"}' http://localhost:8080/api/v1/travelservice/trips/left
+./hey_linux_amd64 -z 30s -c 10 -m POST -H "Content-Type: application/json" -d '{"startingPlace": "Shang Hai", "endPlace": "Su Zhou"}' http://localhost:8080/api/v1/travelservice/trips/left
