141203e127418fc65aae6d4e0a4361cd6216cf6f
mtoslalibu <44147008+mtoslalibu@users.noreply.github.com>
2022-01-26 12:24:10 -0500

    Update setupSocialNetwork.sh

--- setupSocialNetwork.sh
+++ setupSocialNetwork.sh
@@ -26,6 +26,9 @@ cd /local
 git clone https://github.com/mtoslalibu/DeathStarBench.git
 cd DeathStarBench/socialNetwork
 #sudo docker-compose up -d
+## build your own version
+sudo docker build -t yg397/social-network-microservices:latest .
+
 #sudo python3 scripts/init_social_graph.py --graph=socfb-Reed98
 
 ## start workload
