5cc6290a6f29a68043f0e0d9f50454382725baa8
mtoslalibu <44147008+mtoslalibu@users.noreply.github.com>
2022-01-27 15:57:14 -0500

    Create installLibs.sh

---
+++ deathstar/installLibs.sh
@@ -0,0 +1,8 @@
+## isntall requirements
+sudo apt-get --yes install luarocks
+sudo luarocks install luasocket
+sudo apt --yes install python3.8
+sudo apt-get --yes install libssl-dev
+sudo apt-get --yes install libz-dev
+sudo apt-get --yes install python3-pip
+pip3 install aiohttp
