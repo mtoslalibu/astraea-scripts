83161c8d7e86c5312b4d1e5dfdb7a52bc0532184
mtoslalibu <44147008+mtoslalibu@users.noreply.github.com>
2022-01-28 11:33:18 -0500

    Update dockerSetup.sh

--- deathstar/dockerSetup.sh
+++ deathstar/dockerSetup.sh
@@ -33,7 +33,7 @@ sudo systemctl stop docker.service
 sudo systemctl stop docker.socket
 
 SEARCH_STRING="ExecStart=/usr/bin/dockerd -H fd://"
-REPLACE_STRING="ExecStart=/usr/bin/dockerd -g /mydata/docker -H fd://"
+REPLACE_STRING="ExecStart=/usr/bin/dockerd -g /mydata -H fd://"
 sudo sed -i "s#$SEARCH_STRING#$REPLACE_STRING#" /lib/systemd/system/docker.service
 
 sudo rsync -aqxP /var/lib/docker/ /mydata
