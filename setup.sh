392713def32458aecedf1c7d488ecdcf2cf65620
mtoslalibu <44147008+mtoslalibu@users.noreply.github.com>
2022-02-07 16:49:56 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 ## execute this script with bash (i.e., /bin/bash setup.sh)
 
 ## install jdk and maven
-echo "Basladi"
+echo "Basladi" >> /local/mertlogs
 
 sudo apt update
 sudo apt-get --yes install openjdk-8-jdk
@@ -17,20 +17,20 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 sudo docker-compose --version
 
-echo "Docker done"
+echo "Docker done" >> /local/mertlogs
 
 ### create extrafs
 ## stop docker to update work dir
 sudo systemctl stop docker.service
 sudo systemctl stop docker.socket
 
-
-
 ## bash users
 for user in $(ls /users)
 do
 	sudo chsh $user --shell /bin/bash
 done
+
+echo "Checkpoint" >> /local/mertlogs
 
 ## create extrafs
 sudo mkdir /mydata
@@ -42,8 +42,8 @@ sudo sed -i "s#$SEARCH_STRING#$REPLACE_STRING#" /lib/systemd/system/docker.servi
 sudo rsync -aqxP /var/lib/docker/ /mydata
 sudo systemctl daemon-reload
 sudo systemctl start docker
-ps aux | grep -i docker | grep -v grep
-echo "Check above for directory on where docker works"
+ps aux | grep -i docker | grep -v grep >> /local/mertlogs
+echo "Check above for directory on where docker works" >> /local/mertlogs
 
 
 cd /local
@@ -63,6 +63,7 @@ git submodule update
 sudo ./gradlew clean install
 cd ..
 
+echo "Checkpoint jaeger-client-java" >> /local/mertlogs
 
 ## servlet mert’s version
 git clone https://github.com/mtoslalibu/java-web-servlet-filter.git
@@ -71,12 +72,17 @@ git checkout --track origin/v0.1.1-astraea
 sudo ./mvnw clean install -Dlicense.skip=true -Dcheckstyle.skip -DskipTests=true
 cd ..
 
+echo "Checkpoint java-web-servlet-filter" >> /local/mertlogs
+
 ## java spring web mert’s version
 git clone https://github.com/mtoslalibu/java-spring-web.git
 cd java-spring-web
 git checkout --track origin/v-0.3.4-astraea
 sudo ./mvnw clean install -Dlicense.skip=true -Dcheckstyle.skip -DskipTests=true
 cd ..
+
+echo "Checkpoint java-spring-web.git" >> /local/mertlogs
+
 
 ## git clone fork repo of java spring jaeger
 git clone https://github.com/mtoslalibu/java-spring-jaeger.git
@@ -86,6 +92,7 @@ git checkout --track origin/v0.2.2-astraea
 sudo ./mvnw clean install -Dlicense.skip=true -Dcheckstyle.skip -DskipTests=true
 cd ..
 
+echo "Checkpoint java spring jaeger" >> /local/mertlogs
 
 ## go trainticket + switch to jaeger branch + then change java-jaeger-spring version to snapshot
 ##git checkout jaeger
@@ -104,12 +111,12 @@ cp /local/astraea-scripts/astraea-span-allenabled /local/astraea-spans/states
 
 
 #echo "Everything is installed and built now. go ahead and create external fs (mydata)"
-echo "After that please go back to train ticket - and docker-compose build then docker-compose up"
+echo "After that please go back to train ticket - and docker-compose build then docker-compose up" >> /local/mertlogs
 
 ## send email
 mail -s "TrainTicket instance finished setting up!" $(geni-get slice_email)
 
-exit 1
+exit 0
 ## -------------------
 
 #ssh -p 22 toslali@amd133.utah.cloudlab.us -Y -L 16686:localhost:16686
