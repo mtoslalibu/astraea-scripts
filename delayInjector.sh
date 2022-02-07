0fcdab1675a61c0578cbb9310b06407319d5dc72
mtoslalibu <44147008+mtoslalibu@users.noreply.github.com>
2022-02-07 15:34:02 -0500

    Update delayInjector.sh

--- delayInjector.sh
+++ delayInjector.sh
@@ -72,7 +72,7 @@ echo "Time now while injection $(date +%s)"
 # date +%s
 
 ## run workload 
-./hey_linux_amd64 -z 10s -c 6 -q 6 -m POST -H "Content-Type: application/json" -d '{"startingPlace": "Shang Hai", "endPlace": "Su Zhou"}' http://localhost:8080/api/v1/travelservice/trips/left > /tmp/heyyinjected &
+./hey_linux_amd64 -z 30s -c 6 -q 6 -m POST -H "Content-Type: application/json" -d '{"startingPlace": "Shang Hai", "endPlace": "Su Zhou"}' http://localhost:8080/api/v1/travelservice/trips/left > /tmp/heyyinjected &
 workloadPID=$!
 
 
