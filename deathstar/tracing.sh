faa4c59155f3cd290e96710622e2049bc89ced0e
mtoslalibu <44147008+mtoslalibu@users.noreply.github.com>
2022-02-17 10:54:53 -0500

    Create tracing.sh

---
+++ deathstar/tracing.sh
@@ -0,0 +1,5 @@
+wget -O opentracing-cpp-1.5.1.tar.gz https://github.com/opentracing/opentracing-cpp/archive/v1.5.1.tar.gz
+tar -zxf opentracing-cpp-1.5.1.tar.gz
+
+wget -O jaeger-client-cpp-0.4.2.tar.gz https://github.com/jaegertracing/jaeger-client-cpp/archive/v0.4.2.tar.gz
+tar -zxf jaeger-client-cpp-0.4.2.tar.gz
