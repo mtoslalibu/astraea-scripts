480a89dc4794cba1b48a28e491090ffa9bb36084
mtoslalibu <44147008+mtoslalibu@users.noreply.github.com>
2022-04-06 11:43:33 -0400

    Create setup.sh

---
+++ media/setup.sh
@@ -0,0 +1,14 @@
+cd /local/DeathStarBench/mediaMicroservices/docker/thrift-microservice-deps/cpp
+sudo docker build -t mert/thrift-microservice-deps:xenial .
+
+cd /local/DeathStarBench/mediaMicroservices
+sudo docker build -t mert/media-microservices:latest .
+
+sudo docker-compose up -d
+python3 scripts/write_movie_info.py -c datasets/tmdb/casts.json -m datasets/tmdb/movies.json && scripts/register_users.sh && scripts/register_movies.sh
+
+
+cd /wrk2
+sudo make
+./wrk -D exp -t 3 -c 3 -d 3 -L -s ./scripts/media-microservices/compose-review.lua http://localhost:8080/wrk2-api/review/compose -R 10
+
