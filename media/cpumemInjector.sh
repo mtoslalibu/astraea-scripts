f15f92fa9dfc862baa16cdfcf3b481200f19c5bf
mtoslalibu <44147008+mtoslalibu@users.noreply.github.com>
2022-05-11 14:57:32 -0400

    Create cpumemInjector.sh

---
+++ media/cpumemInjector.sh
@@ -0,0 +1,83 @@
+filename_out="/mydata/traces/synexperimentcpumemmedia"
+mkdir -p $filename_out
+
+> /local/astraea-spans/sleeps
+echo "check sleeps now"
+cat /local/astraea-spans/sleeps
+
+iter=8
+duration=150
+qps=20
+
+
+x=0
+while [ $x -lt $iter ]
+do
+
+x=$(( $x + 1 ))
+echo "----------------------------"
+echo -e "\n\nIteration $x"
+sleep 3
+
+filename=`shuf -n 1 /local/astraea-scripts/media/all-spans | tee /local/astraea-spans/sleeps | xargs -n2 -d'\n' | sed -e 's/ /___/g'`
+echo "Running for $filename"
+filename="$filename_out/$filename"
+
+echo "Time now while injection $(date +%s)"
+
+## run workload
+cd /local/DeathStarBench/mediaMicroservices/wrk2
+
+./wrk -D exp -t 5 -c 5 -d $duration -L -s ./scripts/media-microservices/compose-review.lua http://localhost:8080/wrk2-api/review/compose -R $qps > /tmp/heyyinjected &
+workloadPID=$!
+
+echo "Started workload in bg"
+
+cd /local/astraea-scripts/media
+svc=`sed "${x}q;d" services-to-inject`
+echo -e "Service now: $svc"
+
+cd /local
+
+injector=1
+injector_lim=$(( $duration / 30))
+echo -e "Injector times lim: $injector_lim"
+while [ $injector -le $injector_lim ]
+do
+
+echo -e "Injecting nth: $injector"
+sudo ./pumba_linux_amd64 stress --duration 30s --stressors="--matrix 1 -t 30s" $svc
+injector=$(( $injector + 1 ))
+done
+
+echo "Finished injecting"
+
+
+
+wait $workloadPID
+echo -e "\nTime now after injection $(date +%s)"
+
+qpsnow=$((qps-1))
+limit=$((duration*qpsnow))
+cat /tmp/heyyinjected |  tail -6  | head -1
+
+sleep 15
+endtime=`date +%s`
+endtime="${endtime}000000"
+echo "limit after injection now $limit , endtime after injection now $endtime"
+
+url="http://localhost:16686/api/traces?end=$endtime&limit=$limit&maxDuration&minDuration&prettyPrint=true&raw=true&service=compose-review-service"
+echo "curling now to $svc_line with url $url"
+
+filename="$filename-injected"
+curl -sS "$url" > $filename
+echo "saved traces to  $filename"
+
+> /local/astraea-spans/sleeps
+
+sleep 15;
+
+echo "check line number afterwards should be empty"
+cat /local/astraea-spans/sleeps
+
+done
