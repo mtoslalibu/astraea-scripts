92688967cd91e30929fff7e846024b477e405080
mtoslalibu <44147008+mtoslalibu@users.noreply.github.com>
2022-06-09 12:18:17 -0400

    Update workload.sh

--- deathstar/workload.sh
+++ deathstar/workload.sh
@@ -1,3 +1,5 @@
 cd wrk2
 ./wrk -D exp -t 20 -c 20 -d 5 -L -s ./scripts/social-network/compose-post.lua http://localhost:8080/wrk2-api/post/compose -R 1
+#./wrk -D exp -t 30 -c 30 -d 5 -L -s ./scripts/social-network/compose-post.lua http://localhost:8080/wrk2-api/post/compose -R 50
+#./wrk -D exp -t 30 -c 30 -d 100 -L -s ./scripts/social-network/read-user-timeline.lua http://localhost:8080/wrk2-api/user-timeline/read -R 50
 echo "Done"
