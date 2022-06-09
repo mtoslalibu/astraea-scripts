b0197f3b4808b8acbefc10170651a498883d721c
mtoslalibu <44147008+mtoslalibu@users.noreply.github.com>
2022-06-09 11:26:53 -0400

    Update setup.sh

--- media/setup.sh
+++ media/setup.sh
@@ -5,6 +5,7 @@ cd /local/DeathStarBench/mediaMicroservices
 sudo docker build -t mert/media-microservices:latest .
 
 sudo docker-compose up -d
+pip3 install aiohttp
 python3 scripts/write_movie_info.py -c datasets/tmdb/casts.json -m datasets/tmdb/movies.json && scripts/register_users.sh && scripts/register_movies.sh
 
 
